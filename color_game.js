843ff9fec82bf4058697a4d2b3d3ed10c87394f2
ShaurabhTiwari <68499268+ShaurabhTiwari@users.noreply.github.com>
2021-02-15 00:20:55 +0530

    Update color_game.js

--- color_game.js
+++ color_game.js
@@ -88,38 +88,6 @@ modeButtons.forEach((button) => {
     });
 });
 
-/*
-//easy Button
-easyButton.addEventListener("click", function() {
-    this.classList.add("selected");
-    hardButton.classList.remove("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i=0; i<squares.length; i++){
-        if(colors[i]){
-            squares[i].style.backgroundColor = colors[i];
-        }else{
-            squares[i].style.display = "none";
-        }
-    }
-})
-
-//Hard Button
-hardButton.addEventListener("click", function() {
-    this.classList.add("selected");
-    easyButton.classList.remove("selected");
-    numSquares = 6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i=0; i<squares.length; i++){
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "block";
-    }
-})
-*/
 
 for(let i=0; i<squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
